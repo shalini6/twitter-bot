30cf6e9182066aa724b467d02aa767e917135c64
Shalini <slini06@gmail.com>
2017-02-06 22:00:14 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'wStqYUEqYri2zZaBp76pn87Zj',  
+  consumer_secret: 'TrSZXsRyaOPInfkIIAb0EYOHA4nWH1Ohkn59zYjs2MxKkqDVJX',
+  access_token: '476484212-8yQeqFzDyQ88RYWbqnh3aqvaM5tnKONp4wVkZYy4',  
+  access_token_secret: 'QDpfW8ok3auy41XnOyINyfQLKUMAXLyfCwaJQKxOpddAE'
+}
